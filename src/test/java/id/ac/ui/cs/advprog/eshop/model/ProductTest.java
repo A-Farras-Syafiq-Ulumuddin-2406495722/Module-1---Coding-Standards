ce14948e66b66b4e68101af4218ef27287698acb
Rainmo0 <aunknown145@gmail.com>
2026-02-10 17:55:01 +0700

    commiting test model

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/ProductTest.java
@@ -0,0 +1,29 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class ProductTest {
+    Product product;
+    @BeforeEach
+    void setUp() {
+        this.product = new Product();
+        this.product.setProductId("eb558e9f-eak-mantap-gaes-71af6af63bd6");
+        this.product.setProductName("Ayam Geprek Silvanna");
+        this.product.setProductQuantity(500);
+    }
+
+    @Test
+    void testGetProductId() {
+        assertEquals("eb558e9f-eak-mantap-gaes-71af6af63bd6", this.product.getProductId());
+    }
+    @Test
+    void testGetProductName() {
+        assertEquals("Ayam Geprek Silvanna", this.product.getProductName());
+    }
+    @Test
+    void testGetProductQuantity() {
+        assertEquals(500, this.product.getProductQuantity());
+    }
+}
